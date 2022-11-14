a565a0c26ed657b4c9825a1ef0f5a8c0a28ce0e7
Victor Zheng <zhengvictor0@gmail.com>
2022-11-13 21:56:10 -0500

    format and add gitignore

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 
 from csv import reader, writer
 
+
 def get_profile(cfcid):
     URL = f"https://server.chess.ca/api/player/v1/{cfcid}"
     page = requests.get(URL)
@@ -13,28 +14,28 @@ def get_profile(cfcid):
 def read_from_file():
     # file is recommend to be in the following format: starting rank, Name of Player, CFC ID
     # information of the event
-    event_date = datetime(2022,12,11) # date of the event
+    event_date = datetime(2022, 12, 11)  # date of the event
     # event_date = datetime.now() # if the event is today
-    file_path = "resources/holidaysOpenOct13.csv"  # file to read. First 3 csv columns must be what we want to keep
-    cfc_id_index = 2 # index of the id number. Assume everything to the left of the index is what we want to keep
+    file_path = "resources/holidays.csv"  # file to read. First 3 csv columns must be what we want to keep
+    cfc_id_index = 2  # index of the id number. Assume everything to the left of the index is what we want to keep
 
     id_list = []
 
     with open(file_path, 'r') as f:
         csv_reader = reader(f)
-        header = next(csv_reader) # skip the header
+        header = next(csv_reader)  # skip the header
 
         new_csv_rows = []
 
         for row in csv_reader:
             # iterate through each row
             print(row)
-            cfc_id = row[cfc_id_index] # the location of the csv file
+            cfc_id = row[cfc_id_index]  # the location of the csv file
 
-            if not row[0]: continue # skip empty values
+            if not row[0]: continue  # skip empty values
 
-            data_to_write = row[0:cfc_id_index+1] # we want to keep the first few indices
-            if cfc_id != '': # if the id is not empty
+            data_to_write = row[0:cfc_id_index + 1]  # we want to keep the first few indices
+            if cfc_id != '':  # if the id is not empty
                 id_list.append(cfc_id)
 
                 cfc_expiry = get_profile(cfc_id)["player"]["cfc_expiry"]
@@ -56,10 +57,12 @@ def read_from_file():
     print(new_csv_rows)
     f.close()
 
-    new_header = header[0:cfc_id_index+1] + ["CFC Membership"] + ["CFC Rating"]  # new header based on what we want to print
-    sorted_data = sort_by_rating(new_csv_rows, cfc_id_index + 2)  # sort by the index of the rating. Rating index is 2 above the CFC index
+    new_header = header[0:cfc_id_index + 1] + ["CFC Membership"] + [
+        "CFC Rating"]  # new header based on what we want to print
+    sorted_data = sort_by_rating(new_csv_rows,
+                                 cfc_id_index + 2)  # sort by the index of the rating. Rating index is 2 above the CFC index
     print(sorted_data)
-    write_to_file("updated" + file_path, new_header, sorted_data)
+    write_to_file("updated_" + file_path, new_header, sorted_data)
 
 
 def write_to_file(filename, header, contents):
@@ -74,11 +77,11 @@ def write_to_file(filename, header, contents):
 
 def sort_by_rating(data, ratings_index):
     # sorts the data by ratings and updates the rank of the players
-    data.sort(key = lambda row: row[ratings_index], reverse=True)
+    data.sort(key=lambda row: row[ratings_index], reverse=True)
 
     rankings_number = 1
     for line in data:
-        line[0] = rankings_number # assume that the first value is the rankings
+        line[0] = rankings_number  # assume that the first value is the rankings
         rankings_number += 1
 
     return data
